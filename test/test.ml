61b0bd6cfa05f008d275931a5ebeade23697d435
Marshall Roch <mroch@fb.com>
2018-03-15 16:48:58 -0700

    Raise specific exceptions instead of Failure

--- test/test.ml
+++ test/test.ml
@@ -37,6 +37,26 @@ let tests = "vlq" >::: [
         input actual
     ) cases
   end;
+
+  "decode_extra" >:: begin fun ctxt ->
+    let stream = Stream.of_string "qxmvrH the rest is ignored" in
+    let actual = Vlq.Base64.decode stream in
+    assert_equal ~ctxt ~printer:string_of_int 123456789 actual
+  end;
+
+  "decode_eof" >:: begin fun ctxt ->
+    let stream = Stream.of_string "qxmvr" in
+    assert_raises Vlq.Unexpected_eof (fun () ->
+      Vlq.Base64.decode stream
+    )
+  end;
+
+  "decode_invalid" >:: begin fun ctxt ->
+    let stream = Stream.of_string "qx." in
+    assert_raises (Vlq.Invalid_base64 '.') (fun () ->
+      Vlq.Base64.decode stream
+    )
+  end;
 ]
 
 let () = run_test_tt_main tests
